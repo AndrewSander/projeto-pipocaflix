47ca405f0555dae37f7ee37c9ebda9de864f6419
fariassmari <marianafariasbrito@gmail.com>
2025-08-24 20:58:07 -0300

    Revert "Ajeitando traillers e ajustando estilização da página de editar filmes"
    This reverts commit 2a2eda6962be7e0918c50a33acbf070d5182fd7c.

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -173,7 +173,7 @@ def editar_filme(filme_id):
 
         db.session.commit()
         flash("Filme atualizado com sucesso!", "success")
-        return redirect(url_for('admin.lista_filmes'))
+        return redirect(url_for('admin.novo_filme'))
     
     return render_template('editar_filme.html', filme=filme, generos=generos_disponiveis)
 
